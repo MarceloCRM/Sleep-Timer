c87e6f9ca1bdd6f89623e74bb95e28c5f67429c9
MarceloCRM <marcelo.crmoura@gmail.com>
2025-01-13 02:26:35 -0300

    código totalmente refatorado

--- SleepTimer.py
+++ SleepTimer.py
@@ -1,45 +1,36 @@
 import os
 from time import sleep
+from datetime import datetime
 
-valor = True
+hora_atual = datetime.now()
 flag = True
-
-while valor:
+while flag:
     try:
-        pergunta = input('Deseja ativar o sleep timer?: (s/n) ')
-        if pergunta.lower() == 's':
-            while flag:
-                tempo = int(input('Por quanto tempo ele continuará ligado?: (Digite em segundos) '))
-                if tempo <= 0:
-                    print('-'*39)
-                    print('Por favor, insira um valor MAIOR que 0!')
-                    print('-'*39)
-                else:
-                    flag = False
-                    valor = False
-                    if tempo/60 > 9:
-                        print(f'+{"-"*41}+\n|  O tempo definido foi de {tempo/60:.0f} minuto(s)!  |\n+{"-"*41}+\n')
-                    else:
-                        print(f'+{"-"*41}+\n|  O tempo definido foi de {tempo/60:.1f} minuto(s)! |\n+{"-"*41}+\n')
-                    for i in range(tempo, 0, -1):
-                        sleep(1)
-                        if i == 10:
-                            print(f'+{"-"*41}+\n|       Contagem regressiva iniciada!     |\n+{"-"*41}+')
-                            sleep(0.5)
-                            print(f'| Restam {i} segundos para o desligamento! |')
-                        if i < 10:
-                            print(f'| Restam {i} segundos para o desligamento!  |')
-                    sleep(1)
-                    print(f'+{"-"*41}+\n|   Obrigado por usar nosso aplicativo!   |\n+{"-"*41}+')
-                    sleep(2)
-                    os.system("shutdown /s /t 1")
-        elif pergunta.lower() == 'n':
-            print(f'+{"-"*41}+\n|           Você escolheu sair!           |\n+{"-"*41}+')
-            sleep(2)
-            valor = False
+        tempo = input("Até que horas o computador ficará ligado? (HH:MM): ")
+        try:
+            hora_futura = datetime.strptime(f"{tempo}", "%H:%M").replace(year=hora_atual.year, month=hora_atual.month, day=hora_atual.day)
+        except:
+            hora_futura = datetime.strptime(f"{tempo}", "%H:%M:%S").replace(year=hora_atual.year, month=hora_atual.month, day=hora_atual.day)
+        diferenca = int((hora_futura-hora_atual).total_seconds())
+        if diferenca < 1:
+            print(f"+{'-'*36}+\n| Por favor, insira uma hora futura! |\n+{'-'*36}+")
         else:
-            print(f'+{"-"*41}+\n|      Por favor, insira "s" ou "n".      |\n+{"-"*41}+')
+            flag = False
+            message = f"|  O computador será desligado em {diferenca//60} minuto(s), às {str(hora_futura.time())}.  |"
+            print(f"+{'-'*(len(message)-2)}+\n{message}\n+{'-'*(len(message)-2)}+") 
+            for i in range(int(diferenca), 0, -1):
+                message = f"Restam {i} segundos para o desligamento!"
+                if i == 10:
+                    print(f"+{'-'*41}+\n|       Contagem regressiva iniciada!     |\n+{'-'*41}+")
+                    print(f"| {message.ljust(len("|       Contagem regressiva iniciada!     |")-4)} |")
+                elif i < 10:
+                    print(f"| {message.ljust(len("|       Contagem regressiva iniciada!     |")-4)} |")
+                sleep(1)
             sleep(1)
-    except:
-        print(f'+{"-"*41}+\n|      Por favor, insira "s" ou "n".       |\n+{"-"*41}+')
+            print(f"+{'-'*41}+\n|   Obrigado por usar nosso aplicativo!   |\n+{'-'*41}+")
+            sleep(2)
+            os.system("shutdown /s /t 1")
+    except ValueError:
+        print(f"\nErro: O formato não corresponde a HH:MM ou HH:MM:SS.\n")
         sleep(1)
+    
